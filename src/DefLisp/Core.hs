0bbc1a12ae55e73bb9017de1b61ccf5227f37c19
Alex Petrov <alexp@coffeenco.de>
2013-11-16 23:33:10 +0100

    Finally made defined variables and right-hand-side recursive evaluation

--- src/DefLisp/Core.hs
+++ src/DefLisp/Core.hs
@@ -4,12 +4,44 @@ import DefLisp.Core.Types
 import DefLisp.Core.Show
 import DefLisp.Core.Parser
 
+import Text.ParserCombinators.Parsec
+
+import Data.IORef
+import Control.Monad.Error
 -- import Debug.Trace
 
-import Text.ParserCombinators.Parsec
+import qualified Data.HashTable.IO as H
+
+type IOThrowsError = ErrorT LispError IO
+
+fromJust :: Maybe a -> a
+fromJust Nothing = error "Maybe.fromJust: Nothing"
+fromJust (Just x) = x
+
+type Environment k v = H.BasicHashTable k v
+
+type LispEnvironment = (Environment LispExpression LispExpression)
+
+freshEnv :: IO LispEnvironment
+freshEnv = do
+   env <- H.new
+   return env
+
+defineVar :: LispEnvironment -> LispExpression -> LispExpression -> IO (LispExpression)
+defineVar env symbol expr = do
+  H.insert env symbol expr
+  return symbol
+
+findVar :: Environment LispExpression LispExpression -> LispExpression -> IO (Maybe LispExpression)
+findVar env symbol = do
+  res <- H.lookup env symbol
+  return res
+
+-- bar :: IO (HashTable Int Int) ->
 
 unpackNum :: LispExpression -> Integer
 unpackNum (LispNumber n) = n
+unpackNum (LispList [n]) = unpackNum n
 unpackNum (LispString s) = read s :: Integer
 
 --
@@ -19,23 +51,49 @@ unpackNum (LispString s) = read s :: Integer
 numericOp :: (Integer -> Integer -> Integer) -> [LispExpression] -> LispExpression
 numericOp op args = LispNumber $ foldl1 op $ map unpackNum args
 
-builtInOp :: String -> [LispExpression] -> LispExpression
+builtInOp :: String -> [LispExpression] -> IO LispExpression
 -- builtInOp "+" args | trace(show $ foldl1 (+) $ map unpackNum args) False = undefined
-builtInOp "+" args = numericOp (+) args
-builtInOp "-" args = numericOp (-) args
-builtInOp "*" args = numericOp (*) args
-builtInOp "/" args = numericOp (div) args
+builtInOp "+" args = return $ numericOp (+) args
+builtInOp "-" args = return $ numericOp (-) args
+builtInOp "*" args = return $ numericOp (*) args
+builtInOp "/" args = return $ numericOp (div) args
 
-eval :: LispExpression -> LispExpression
-eval val@(LispString _) = val
-eval val@(LispSymbol _) = val
--- eval val@(LispList _) = val
--- eval (LispList (LispSymbol _: args)) = LispNumber $ foldl1 (+) $ map unpackNum args
-eval (LispList (LispSymbol func: args)) = builtInOp func args
-eval val@(LispNumber _) = val
-eval val@(LispBool _) = val
+eval :: LispEnvironment -> LispExpression -> IO LispExpression
+eval _ val@(LispString _) = return val
+eval _ val@(LispSymbol _) = return val
 
+eval envRef (LispList[LispSymbol "def", LispSymbol var, form]) =
+-- eval envRef form >>= defineVar envRef var
+  eval envRef form >>= defineVar envRef (toSexp var)
+-- defineVar envRef var form
+
+-- eval env (List [Atom "define", Atom var, form]) =
+--     eval env form >>= defineVar env var
+
+eval envRef (LispList (LispSymbol func: args)) =
+  mapM (eval envRef) args >>= builtInOp func
+
+eval _ val@(LispNumber _) = return val
+eval _ val@(LispBool _) = return val
 
 -- evalExpression :: String -> String
-
 -- (eval . readExpression) "(+ 1 1)"
+
+t :: String -> IO LispEnvironment
+t exp = do
+  -- ht <- freshEnv
+  ht <- H.new
+  let sym = mklSymbol "sym"
+      num = mklNumber 1
+      read = readExpression exp
+      evaled = eval ht read
+  --defineVar ht sym num
+  --lookedup <- findVar ht sym
+  --print $ (fromJust lookedup)
+  s <- liftM show evaled
+  lookedup <- findVar ht sym
+  print $ (fromJust lookedup)
+  print s
+
+
+  return ht
--- src/DefLisp/Core/Types.hs
+++ src/DefLisp/Core/Types.hs
@@ -1,19 +1,32 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module DefLisp.Core.Types where
 
+import GHC.Generics (Generic)
 import qualified Data.Map as Map
+import Data.Hashable
 
 import Control.Monad.Error
-import Control.Monad.State
+-- import Control.Monad.State
 
 
 
 type SymbolTable = Map.Map String LispExpression
 data Context = Ctx SymbolTable (Maybe Context)
 
+type ThrowsError = Either LispError
 
+data LispError = NumArgs Integer [LispExpression]
+               | TypeMismatch String LispExpression
+               | UnboundVar String String
+               | Default String
 
-type LispError = ErrorT String IO
-type LispResult = StateT Context LispError LispExpression
+instance Error LispError where
+    noMsg = Default "An error has occurred"
+    strMsg = Default
+
+-- type LispResult = StateT Context LispError LispExpression
 
 
 data LispExpression = LispSymbol String |
@@ -21,4 +34,39 @@ data LispExpression = LispSymbol String |
                       LispNumber Integer |
                       LispString String |
                       LispBool Bool
-                      deriving (Show)
+                      deriving (Show, Eq, Generic)
+
+instance Hashable LispExpression where
+  hashWithSalt s (LispSymbol n) = s `hashWithSalt`
+                                  (0::Int) `hashWithSalt` n
+
+  hashWithSalt s (LispNumber n) = s `hashWithSalt`
+                                  (0::Int) `hashWithSalt` n
+
+mklNumber :: Integer -> LispExpression
+mklNumber n = LispNumber n
+
+mklSymbol :: String -> LispExpression
+mklSymbol n = LispSymbol n
+
+mklString :: String -> LispExpression
+mklString n = LispString n
+
+class LispLiteral l where
+  toSexp :: l -> LispExpression
+
+instance LispLiteral Integer where
+  toSexp n = LispNumber n
+
+instance LispLiteral [Char] where
+  toSexp n = LispSymbol n
+
+instance LispLiteral Bool where
+  toSexp n = LispBool n
+
+instance LispLiteral [Integer] where
+  toSexp n = LispList $ map toSexp n
+
+mklList :: [Integer] -> LispExpression
+mklList a = LispList $ map toSexp a
+-- LispList [1,2,3]
