c1dafb2e6dff426d341cdfa5378334e25902796a
Alex Petrov <alexp@coffeenco.de>
2013-11-28 23:03:16 +0100

    Change show to more proper implementation

--- src/DefLisp/Core/Show.hs
+++ src/DefLisp/Core/Show.hs
@@ -2,13 +2,14 @@ module Deflisp.Core.Show where
 
 import Deflisp.Core.Types
 
-unwordsList :: [LispExpression] -> String
-unwordsList = unwords . map showVal -- ()
 
-showVal :: LispExpression -> String
-showVal (LispNumber x) = show x
-showVal (LispSymbol x) = "\'" ++ x
-showVal (LispList x) = "(" ++ unwordsList x ++ ")"
-showVal (LispFunction _ _) = "fn"
-showVal (LispString x) = "\"" ++ x ++ "\""
-showVal (LispBool _) = ""
+instance Show LispExpression where
+  show (LispNumber n) = show n
+  show (LispSymbol n) = n
+  show (ReservedKeyword n) = show n
+  show (LispList n) = "(" ++ (unwords (map show n)) ++ ")"
+  show (LispVector n) = "(" ++ (unwords (map show n)) ++ ")"
+  show (LispString n) = n
+  show (LispBool n) = show n
+  show (LispFunction bindings expr) = "(fn [" ++ (show bindings) ++ "] " ++ (show expr) ++ ")"
+  show (LispNil) = "nil"
